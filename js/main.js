834c594f6043121aa5ce422c29e74a68f7deabe5
Mavlon4658 <72940597+Mavlon4658@users.noreply.github.com>
2024-04-26 02:46:44 +0500

    yangilandi

--- js/main.js
+++ js/main.js
@@ -135,3 +135,135 @@ function bodyHidden () {
 function bodyVisible () {
     document.querySelector('body').style.overflow = 'visible'
 }
+
+let modal = document.querySelectorAll('.modal'),
+    modalClose = document.querySelectorAll('.modal__close');
+    
+modalClose.forEach(btn => {
+    btn.onclick = () => {
+        modal.forEach(m => {
+            m.classList.remove('active');
+        })
+    }
+})
+
+for (let i = 1; i <= 5; i++) {
+    let currentModal = document.querySelector(`.modal_${i}`),
+        modalOpen = document.querySelectorAll(`.modal_${i}_open`);
+
+    if (modalOpen.length) {
+        modalOpen.forEach(btn => {
+            btn.onclick = e => {
+                e.preventDefault();
+                currentModal.classList.add('active');
+            }
+        })
+    }
+
+}
+
+let passWrap = document.querySelectorAll('.password_wrap');
+
+if (passWrap.length) {
+    passWrap.forEach(el => {
+        let inp = el.querySelector('input[type="password"]'),
+            btn = el.querySelector('button');
+        btn.onclick = () => {
+            btn.classList.toggle('active');
+            if (inp.getAttribute('type') == "password") {
+                inp.setAttribute('type', 'text');
+            } else {
+                inp.setAttribute('type', 'password')
+            }
+        }
+    })
+}
+
+let rateWrap = document.querySelectorAll('.rate_wrap');
+
+if (rateWrap.length) {
+    rateWrap.forEach(rate => {
+        let inp = rate.querySelectorAll('input[type="radio"]'),
+            btn = rate.querySelectorAll('.stars svg');
+        
+        btn.forEach((el, idx) => {
+            el.onclick = () => {
+                inp[idx].click();
+                btn.forEach((b, bID) => {
+                    if (idx >= bID) {
+                        b.classList.add('active');
+                    } else {
+                        b.classList.remove('active')
+                    }
+                })
+            }
+        })
+    })
+}
+
+let lifeSwiper = new Swiper('.life .swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    breakpoints: {
+        1300: {
+            slidesPerView: 4,
+            loop: false
+        },
+        992: {
+            slidesPerView: 3,
+            loop: true,
+        },
+        800: {
+            slidesPerView: 2,
+            loop: true,
+        },
+    },
+    navigation: {
+        nextEl: ".life .swp_btn_next",
+        prevEl: ".life .swp_btn_prev"
+    },
+    pagination: {
+        el: ".life .swiper_pagination",
+        clickable: true,
+    }
+})
+
+let plotSwiper = new Swiper('.plot .swiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    navigation: {
+        nextEl: ".plot .swiper_btn_next",
+        prevEl: ".plot .swiper_btn_prev"
+    },
+    pagination: {
+        el: ".plot .swiper_pagination",
+        clickable: true,
+    }
+})
+
+let peopleSwiper = new Swiper('.people .swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    breakpoints: {
+        1300: {
+            slidesPerView: 4,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+        800: {
+            slidesPerView: 2,
+        },
+    },
+    navigation: {
+        nextEl: ".people .swp_btn_next",
+        prevEl: ".people .swp_btn_prev"
+    },
+    pagination: {
+        el: ".people .swiper_pagination",
+        clickable: true,
+    }
+})
---
+++ js/map.js
@@ -0,0 +1,12 @@
+// function init () {
+//     const map = new ymaps.Map('map-demo', {
+//         center: [37.64, 55.76],
+//         zoom: 10,
+//         // type: 'yandex#satellite',
+//         // Карта будет создана без
+//         // элементов управления.
+//         controls: []
+//     });
+// }
+
+// ymaps.ready(init)
